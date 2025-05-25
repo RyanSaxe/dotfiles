f8c186941914bf421d67d86c1be9c0282c6c07fe
Ryan Saxe <ryancsaxe+1@gmail.com>
2025-05-24 23:15:44 -0400

    customizing file saving

---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,19 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    windows = {
+      preview = true,
+      width_focus = 30,
+      width_preview = 100,
+    },
+    options = {
+      use_as_default_explorer = true,
+      -- move to trash instead of permanent delete
+      permanent_delete = false,
+    },
+    mappings = {
+      go_in_plus = "<CR>",
+      synchronize = "w",
+    },
+  },
+}
