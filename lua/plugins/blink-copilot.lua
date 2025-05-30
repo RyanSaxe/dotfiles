0956983d70a07f7700f0468ccc10180d101ffff6
Ryan Saxe <ryancsaxe+1@gmail.com>
2025-05-29 21:11:27 -0400

    adding ai plugins

---
+++ lua/plugins/blink-copilot.lua
@@ -0,0 +1,35 @@
+-- integrating github copilot autocomplete inside the normal LSP/blink autocomplete menu
+-- got this directly from the README: https://github.com/fang2hou/blink-copilot?tab=readme-ov-file
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+}
