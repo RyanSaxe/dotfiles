1fcdcf2b74c5d938f165ea8b5f79f41f1e9fdbf9
RyanSaxe <ryancsaxe+1@gmail.com>
2025-08-31 12:33:01 -0400

    disabling tiny inline

--- nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -1,14 +1,14 @@
 return {
   -- TODO: explore this plugin.
-  enabled = true,
+  enabled = false,
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "VeryLazy", -- Or `LspAttach`
   priority = 1000, -- needs to be loaded in first
   config = function()
     require("tiny-inline-diagnostic").setup()
-    vim.diagnostic.config({ 
+    vim.diagnostic.config({
       virtual_text = false,
-      signs = false -- disable diagnostic signs to eliminate signcolumn padding
+      signs = false, -- disable diagnostic signs to eliminate signcolumn padding
     })
   end,
 }
