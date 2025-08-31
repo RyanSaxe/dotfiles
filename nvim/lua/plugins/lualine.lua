a686915902e77d1e7e0760bc4643f9dc12261265
RyanSaxe <ryancsaxe+1@gmail.com>
2025-08-30 22:15:07 -0400

    simplifying lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -44,35 +44,11 @@ return {
               error = icons.diagnostics.Error,
               warn = icons.diagnostics.Warn,
               info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
             },
           },
         },
         lualine_y = {
-          Snacks.profiler.status(),
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = function() return { fg = Snacks.util.color("Statement") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = function() return { fg = Snacks.util.color("Constant") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = function() return { fg = Snacks.util.color("Debug") } end,
-          },
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = function() return { fg = Snacks.util.color("Special") } end,
-          },
           {
             "diff",
             symbols = {
@@ -92,15 +68,6 @@ return {
             end,
           },
         },
-        -- lualine_y = {
-        --   { "progress", separator = " ", padding = { left = 1, right = 0 } },
-        --   { "location", padding = { left = 0, right = 1 } },
-        -- },
-        -- lualine_z = {
-        --   function()
-        --     return " " .. os.date("%R")
-        --   end,
-        -- },
       },
       extensions = { "neo-tree", "lazy", "fzf" },
     }
