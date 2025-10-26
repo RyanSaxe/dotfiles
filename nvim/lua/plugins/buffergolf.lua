26a4270317e494ae01b4c3aa4cdf6e8ffe45190c
Ryan Saxe <ryancsaxe+1@gmail.com>
2025-10-26 11:33:17 -0400

    adding temp plugin

---
+++ nvim/lua/plugins/buffergolf.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    -- "ryansaxe/keymash.nvim",
+    dir = "~/Projects/neovim/buffergolf.nvim",
+    opts = {
+      -- your override options here
+    },
+    config = function(_, opts)
+      require("buffergolf").setup(opts)
+
+      local group = vim.api.nvim_create_augroup("BufferGolfCopilotMute", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+        group = group,
+        callback = function(event)
+          local buf = event.buf
+          local ok, is_practice = pcall(vim.api.nvim_buf_get_var, buf, "buffergolf_practice")
+          if ok and is_practice then
+            vim.b[buf].copilot_enabled = false
+          else
+            pcall(vim.api.nvim_buf_del_var, buf, "copilot_enabled")
+          end
+        end,
+      })
+    end,
+  },
+}
